5d444bfe074cd93123c7a6049c29a9b410b80a43
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-10-12 19:40:21 -0300

    Creating welcome message

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define ANSI_GREEN "\x1b[32m"
+
+int main() {
+    
+    printf(ANSI_GREEN "SISTEMA PARA ANÁLISE DE PERFORMANCE DE ALGORITMOS DE ORDENAÇÃO DE DADOS\n" ANSI_GREEN);
+    
+    return 0;
+}
