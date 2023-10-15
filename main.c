26d5f738bfa02d4bf213f7b66220b65512ab5c2f
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-10-14 23:44:00 -0300

    Creating the menu and refactoring

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
 
 #define ANSI_GREEN "\x1b[32m"
 #define ANSI_YELLOW "\x1b[33m"
@@ -7,35 +8,46 @@
 #define ANSI_RESET "\x1b[0m"
 #define ANSI_BLUE "\x1b[34m"
 
+void printArray(int *array, int length) {
+
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n\n");
+}
 
 void header(){
-
-    system("clear");
-    printf("==================================================================================================\n");
+    printf("========================================================================\n");
     printf(ANSI_GREEN "SISTEMA PARA ANÁLISE DE PERFORMANCE DE ALGORITMOS DE ORDENAÇÃO DE DADOS\n" ANSI_RESET);
-    printf("==================================================================================================\n\n");
+    printf("========================================================================\n\n");
 }
 
 int sortingTypeChoise(){
+
     printf(ANSI_BLUE "1- BubbleSort\n" ANSI_RESET);
     printf(ANSI_YELLOW "2- QuickSort\n" ANSI_RESET);
     printf(ANSI_RED "3- SelectionSort\n\n" ANSI_RESET);
     printf("Qual tipo de ordenação você deseja analisar?\n");
+
     int choice;
     scanf("%d", &choice);
+    system("clear");
     return choice;
     
 }
 
 int arrayDefinitionChoise(){
 
-    system("clear");
+    printf("Digite uma das opções abaixo:\n\n");
+    printf(ANSI_BLUE "1- Valores aleatórios\n" ANSI_RESET);
+    printf(ANSI_YELLOW "2- Arquivo de texto\n" ANSI_RESET);
+    printf(ANSI_RED "3 - Sair \n\n" ANSI_RESET);
 
-    printf(ANSI_BLUE "1- Arquivo de texto\n" ANSI_RESET);
-    printf(ANSI_YELLOW "2- Valores aleatórios\n\n" ANSI_RESET);
-    printf("Como deseja obter dados?\n");
     int arrayDefinition;
     scanf("%d",&arrayDefinition);
+
+    system("clear");
 
     return arrayDefinition;
 
@@ -62,11 +74,62 @@ void bubbleSort(int *array, int length) {
 }
 
 
-int main() {
-    header();
+int *creatingArrayRandValues(length){
+    int *array;
 
-    int sorting = sortingTypeChoise();
-    int arrayDefinition = arrayDefinitionChoise();
+    array = (int *)malloc(length * sizeof(int));
 
+    for (int i = 0; i < length; i++){
+        array[i] = (rand() % 100) + 1;
+    }
+
+    return array;
     
+}
+
+int readingArrayLength(){
+    int length;
+    printf("Digite o tamanho do array:\n");
+    scanf("%d", &length);
+    return length;
+}
+
+
+int main() {
+    while(1){
+
+        header();
+        int arrayDefinition = arrayDefinitionChoise();
+
+        switch(arrayDefinition){
+            case 1:
+
+                int sorting = sortingTypeChoise();
+                int length = readingArrayLength();
+                int *array = creatingArrayRandValues(length);
+
+                system("clear");
+
+                printf(ANSI_GREEN "Dados iniciais:\n" ANSI_RESET);
+                printArray(array,length);
+
+                if(sorting == 1){
+                    bubbleSort(array, length);
+                    printf(ANSI_BLUE "Dados ordenados com BubbleSort:\n" ANSI_RESET);
+                    printArray(array,length);
+                    free(array);
+                }
+
+                break;
+            case 2:
+                break;
+            case 3:
+                exit(0);
+            
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+        }
+
+    }
 }
